5b8b3f3c5e5b3320a9fcd41c4001645ba87d07ad
Jeremiah_G <gutierrez_outlook.com>
2021-06-04 12:08:21 -0500

    Create List Method w/ variables & Function to Return HTML

--- Program.cs
+++ Program.cs
@@ -8,10 +8,22 @@ namespace Scraper {
 
   class Program 
   {
+    // This is a Global Variable that we will be using for scraping
       static ScrapingBrowser _scrapingBrowser = new ScrapingBrowser();
-    static void Main(string[] args) {
-      Console.WriteLine("Hello World!"); 
-    
+
+
+    // MAINT METHOD //
+    static void Main(string[] args) 
+    {
+        // Paste in to GetHtml whatever website it is you are trying to parse
+        // I chose a url with a list of lesson & tutoring in the mcallen/edinburg area
+        GetHtml("https://mcallen.craigslist.org/d/lessons-tutoring/search/lss");
     } 
+
+    // This is the Function that will return to us the html of a given page
+    static HtmlNode GetHtml(string url) {
+        WebPage webpage = _scrapingBrowser.NavigateToPage(new Uri(url));
+        return webpage.Html; 
+    }
   } 
 }
