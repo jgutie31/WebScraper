2dc7045dd80026ca588f71637ad31babff55cef3
Jeremiah_G <gutierrez_outlook.com>
2021-06-04 13:38:28 -0500

    Copy & Paste of Creators Complete Code

--- Program.cs
+++ Program.cs
@@ -3,48 +3,86 @@ using System.Collections.Generic;
 using HtmlAgilityPack;
 using ScrapySharp.Extensions;
 using ScrapySharp.Network;
+using System.IO;
+using System.Globalization;
+using CsvHelper;
 
-namespace Scraper { 
-
-  class Program 
-  {
-    // This is a Global Variable that we will be using for scraping
-      static ScrapingBrowser _browser = new ScrapingBrowser();
-
-
-    // MAINT METHOD //
-    static void Main(string[] args) 
+namespace Scraper
+{
+    class Program
     {
-        // Paste in to GetHtml whatever website it is you are trying to parse
-        // I chose a url with a list of lesson & tutoring in the mcallen/edinburg area
-        GetMainPageLinks("https://mcallen.craigslist.org/d/lessons-tutoring/search/lss");
-    } 
+        static ScrapingBrowser _browser = new ScrapingBrowser();
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter Search Term:");
+            var searchTerm = Console.ReadLine();
+            var mainPageLinks = GetMainPageLinks("https://newyork.craigslist.org/d/computer-gigs/search/cpg");
+            var lstGigs = GetPageDetails(mainPageLinks, searchTerm);
+            exportGigsToCSV(lstGigs, searchTerm);
+        }
 
-    // Creating another method that is going to return a list of string
+        static List<string> GetMainPageLinks(string url)
+        {
+            var mainPageLinks = new List<string>();
 
-    static List<string> GetMainPageLinks(string url) {
-        // This is where we are going to store the list of urls to ALL of those CraigsList listings
-        var homePageLinks = new List<string>();
-        // This will get the HTML from each link
-        var html = GetHtml(url);
-        // This should grab us ALL OF THE LINKS that are on the page
-        var links = html.CssSelect("a");
+            var html = GetHtml(url);
 
-        // for each link we pull, if "href" contains ".html" then pull it (finding verified pages)
-        foreach (var link in links) {
-            if(link.Attributes["href"].Value.Contains(".hmtl")) {
-                homePageLinks.Add(link.Attributes["href"].Value);
+            var links = html.CssSelect("a");
+
+            foreach (var link in links)
+            {
+                if (link.Attributes["href"].Value.Contains(".html"))
+                {
+                    mainPageLinks.Add(link.Attributes["href"].Value);
+                }
+            }
+
+            return mainPageLinks;
+        }
+
+        static List<PageDetails> GetPageDetails(List<string> urls, string searchTerm)
+        {
+            var lstPageDetails = new List<PageDetails>();
+            foreach (var url in urls)
+            {
+                var htmlNode = GetHtml(url);
+                var pageDetails = new PageDetails();
+                pageDetails.title = htmlNode.OwnerDocument.DocumentNode.SelectNodes("//html/head/title")[0].InnerText;
+                
+                var description = htmlNode.OwnerDocument.DocumentNode.SelectNodes("//html/body/section/section/section/section")[0].InnerText;
+                pageDetails.description = description
+                    .Replace("\n        \n            QR Code Link to This Post\n            \n        \n", "");
+
+                pageDetails.url = url;
+
+                if (pageDetails.title.ToLower().Contains(searchTerm.ToLower())
+                || pageDetails.description.ToLower().Contains(searchTerm.ToLower()))
+                    lstPageDetails.Add(pageDetails);
+            }
+
+            return lstPageDetails;
+        }
+
+        static void exportGigsToCSV(List<PageDetails> lstPageDetails, string searchTerm)
+        {
+            using (var writer = new StreamWriter($@"/Users/robertlewis/Desktop/{searchTerm}_{DateTime.Now.ToFileTime()}.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(lstPageDetails);
             }
         }
-        // return or output 'homePageLinks', which is where all that info will be output
-        return homePageLinks;
+
+        static HtmlNode GetHtml(string url)
+        {
+            WebPage webpage = _browser.NavigateToPage(new Uri(url));
+            return webpage.Html;
+        }
     }
 
-
-    // This is the Function that will return to us the html of a given page
-    static HtmlNode GetHtml(string url) {
-        WebPage webpage = _browser.NavigateToPage(new Uri(url));
-        return webpage.Html; 
+    public class PageDetails
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
     }
-  } 
 }
